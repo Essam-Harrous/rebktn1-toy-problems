be41b5a82da8f63784087f842dfa338488e48f45
essam <esamfarg91@gmail.com>
2019-12-24 09:57:48 +0100

    i'll do it

--- balancedBrackets/balancedBrackets.js
+++ balancedBrackets/balancedBrackets.js
@@ -9,4 +9,46 @@ isBalanced("(50)(") // false
 isBalanced("[{]}") // false
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+  var count = 0;
+  var pcs = {
+    '{' : '}',
+    '}' : '{',
+    countCu: 0,
+    '(' : ')',
+    ')' : '(',
+    countPa: 0,
+    '[' : ']',
+    ']' : '[',
+    countSe: 0
+  }
+
+  for (let i = 0; i < str.length; i++){
+    if(pcs[str[i]]) {
+      switch(str[i]){
+        case '(': pcs.countPa += 1;
+        break;
+        case '[': pcs.countSe += 1;
+        break;
+        case '{': pcs.countSe += 1;
+        break;
+        case ')': pcs.countPa -= 1;
+        break;
+        case ']': pcs.countSe -= 1;
+        break;
+        case '}': pcs.countSe -= 1;
+      }
+      if(pcs.countPa < 0 || pcs.countCu < 0 || pcs.countSe < 0){
+        return false;
+      }
+      if(pcs[str[i]] == '[' || pcs[str[i]] == '(' || pcs[str[i]] == '{' ) {
+         return isBalanced(str.slice(str.indexOf(pcs[str[i]]) + 1,pcs[str[i]]))
+      }
+    }
+  }
+  if(pcs.countPa > 0 || pcs.countCu > 0 || pcs.countSe > 0){
+    return false;
+  }
+  return true;
+
+};
