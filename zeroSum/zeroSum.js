3ebb2eb0002e980168dba52dce8ef241569f662d
essam <esamfarg91@gmail.com>
2019-12-31 09:08:43 +0100

    done

--- zeroSum/zeroSum.js
+++ zeroSum/zeroSum.js
@@ -7,3 +7,13 @@ Constraints: Solve this linear time complexity O(n)
 zeroSum([ 1, 3, 2, -3 ]) // true
 zeroSum([ 5, 7, 2, 9 ]) // false
 */
+
+
+let zeroSum = (arr) => {
+  obj = {};
+  for(var key in arr) {
+    if(obj[Math.abs(arr[key])] != undefined && arr[key] + obj[Math.abs(arr[key])] == 0) return true;
+    else obj[Math.abs(arr[key])] = arr[key]
+  }
+  return false
+}
